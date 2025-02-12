47ced7c3a78dce52d6ba7f8cb2543a46abee5fbb
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-02-11 20:38:01 -0600

    Update PayrollLab.cpp

--- PayrollLab.cpp
+++ PayrollLab.cpp
@@ -1,5 +1,5 @@
-
 #include <iostream>
+#include <iomanip>
 
 double workerhours;
 int workerchild;
@@ -9,12 +9,16 @@ double gross;
 bool InvalidInput = true;
 
 double workermath;
+double ssdeduction;
+double fideduction;
+double sideduction;
+double netpay;
 
 int main()
 {
     std::cout << "Hello! Welcome to the Payroll Program!\n";
     std::cout << "Please enter positive numerical numbers for the following prompts...\n";
-    
+
     while (InvalidInput) {
         std::cout << "\nHow many hours have you worked this week: ";
         std::cin >> workerhours;
@@ -47,33 +51,78 @@ int main()
 
     }
 
+
+
     if (workerhours > 40) {
         gross = (40 * 16.78);
         workermath = (workerhours - 40);
-        gross = (gross + (workerhours * (16.78 * 1.5)))
+        gross = (gross + (workerhours * (16.78 * 1.5)));
 
-    
+
     }
     else {
         gross = (workerhours * 16.78);
-    
+
     }
+
+    std::cout << std::endl << std::endl;
+    std::cout << "Payroll Stub:" << std::endl;
     
+    std::cout << std::endl;
+    std::cout << std::setw(20) << "Hours Worked: ";
+    std::printf("%.2f", workerhours);
+    std::cout << std::endl;
+   
 
-    std::cout << "\n\nPayroll Stub:" << std::endl;
+    std::cout << std::setw(20) << "Rate of Pay: ";
+    std::cout << "16.78 $ / hr" << std::endl;
 
-    std::cout << "\nHours Worked: ";
-//    std::cout.precision(2);
-//    std::cout.width(7);
-    std::cout << workerhours << std::endl;
-    std::cout << "Rate of Pay: ";
-//    std::cout.width(7);
-    std::cout << "16.78 $/hr" << std::endl;
-//    std::cout.width(7);
-    std::cout << "Gross: ";
-    std::cout << gross << std::endl;
+    std::cout << std::setw(20) << "Gross Amount: ";
+    std::printf("$ %.2f", gross);
+    //std::cout << "\nTest Gross: $ " << gross << std::endl;
 
-    std::cout << "SocSec: ";
+
+    //deductions
+
+    ssdeduction = (.06 * gross);
+    fideduction = (.14 * gross);
+    sideduction = (.05 * gross);
+
+    std::cout << std::endl << std::endl << std::endl;
+    std::cout << std::setw(20) << "Deductions: " << std::endl;
+    std::cout << std::setw(20) << "SocialSecruity: ";
+    std::printf("$ %.2f", ssdeduction);
+    std::cout << std::endl;
+    std::cout << std::setw(20) << "Federal Tax: ";
+    std::printf("$ %.2f", fideduction);
+    std::cout << std::endl;
+    std::cout << std::setw(20) << "State Tax: ";
+    std::printf("$ %.2f", sideduction);
+    std::cout << std::endl;
+    std::cout << std::setw(20) << "Union Fee: ";
+    std::cout << "$ 10.00";
+
+    if (workerchild > 2) {
+        std::cout << std::endl;
+        std::cout << std::setw(20) << "Insurance: ";
+        std::cout << "$ 35.00";
+        netpay = (gross - 35);
+    }
+    else {
+        std::cout << std::endl;
+        std::cout << std::setw(20) << "Insurance: ";
+        std::cout << "$ 15.00";
+        netpay = (gross - 15);
+    }
+    netpay = netpay - 10 - ssdeduction - fideduction - sideduction;
+
+    std::cout << std::endl << std::endl;
+    std::cout << std::setw(20) << "Net Pay: ";
+    std::printf("$ %.2f", netpay);
+
+    std::cout << "\n\nThank for using the Payroll Program!" << std::endl;
+
+    return 0;
 
 
 }
